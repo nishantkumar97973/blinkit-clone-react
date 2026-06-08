7b5ac69224ecf77163195a941962f57feb00b006
Nishant Kumar <nishantkumar97973@gmail.com>
2026-06-08 11:39:34 +0530

    Use Aiven MySQL

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,11 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "2580@36",
-  database: "blinkit_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
